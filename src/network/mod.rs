0a916a863a272aebe7215f5c1206479eef815bb6
Monti-27 <msaini27004@gmail.com>
2026-02-03 12:00:00 +0530

    export network types for external use

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -3,3 +3,10 @@
 
 pub mod storage;
 pub mod service;
+
+// re-export key types for convenience
+pub use storage::ShareStore;
+pub use service::CustodianService;
+pub use service::proto::custodian_server::CustodianServer;
+pub use service::proto::custodian_client::CustodianClient;
+pub use service::proto::{ShareData, RetrieveRequest, JoinRequest};
